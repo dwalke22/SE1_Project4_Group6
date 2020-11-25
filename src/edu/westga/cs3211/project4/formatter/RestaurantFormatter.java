82004121edfbaa3cd5551ffb9a7f633c7213021c
Sam Morrell <smorrel1@my.westga.edu>
2020-11-24 21:29:15 -0500

    Added RestaurantFormatter
    Will format the non menu information of a restaurant to be displayed to
    the user in the UI.

---
+++ src/edu/westga/cs3211/project4/formatter/RestaurantFormatter.java
@@ -0,0 +1,18 @@
+package edu.westga.cs3211.project4.formatter;
+
+import edu.westga.cs3211.project4.model.Restaurant;
+
+public class RestaurantFormatter {
+
+	public RestaurantFormatter() {
+		
+	}
+	
+	public String FormatRestaurant(Restaurant restaurant) {
+		String name = restaurant.getName();
+		String distance = " is "+(Math.round(restaurant.getDistance()*100.0)/100.0) + " miles away ";
+		String reviewScore = "with a review score of " + restaurant.getReviewScore() + " stars.";
+		
+		return name + distance + reviewScore;
+	}
+}
