2fe3f852595636e6ef762c84bd6cb6a127d585a9
David Walker <thewalker.david@gmail.com>
2020-11-25 20:08:04 -0500

    Created ViewModel, changed comboboxes to text fields in gui

--- src/view/GUICodeBehind.java
+++ src/view/GUICodeBehind.java
@@ -4,62 +4,59 @@ import edu.westga.cs3211.project4.model.Restaurant;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class GUICodeBehind {
 
-	@FXML
-    private ListView<Restaurant> resturantListView;
+	 @FXML
+	 private ListView<Restaurant> resturantListView;
 
-    @FXML
-    private ComboBox<?> priceRangeComboBox;
-    
-    @FXML
-    private ComboBox<?> distanceComboBox;
+	 @FXML
+	 private TextField priceTextField;
 
-    @FXML
-    private ComboBox<?> reviewScoreComboBox;
-    
-    @FXML
-    private ComboBox<?> foodTypeComboBox;
+	 @FXML
+	 private TextField ratingTextField;
 
-    @FXML
-    private Button applyFilterButton;
+	 @FXML
+	 private Button applyFilterButton;
 
-    @FXML
-    private Button resetFiltersButton;
+	 @FXML
+	 private Button resetFiltersButton;
 
-    @FXML
-    private Button loadAllResturantsButton;
+	 @FXML
+	 private Button loadAllResturantsButton;
 
-    @FXML
-    private Button loadAllMenusButton;
+	 @FXML
+	 private Button loadAllMenusButton;
 
-    @FXML
-    private TextArea resturantTextArea;
+	 @FXML
+	 private TextArea resturantTextArea;
 
-    @FXML
-    private TextArea menuTextArea;
+	 @FXML
+	 private TextArea menuTextArea;
 
-    @FXML
-    void handleApplyFilters(ActionEvent event) {
+	 @FXML
+	 private TextField distanceTextField;
 
-    }
+	 @FXML
+	 void handleApplyFilters(ActionEvent event) {
 
-    @FXML
-    void handleLoadAllMenues(ActionEvent event) {
+	 }
 
-    }
+	 @FXML
+	 void handleLoadAllMenues(ActionEvent event) {
 
-    @FXML
-    void handleLoadAllResturants(ActionEvent event) {
+	 }
 
-    }
+	 @FXML
+	 void handleLoadAllResturants(ActionEvent event) {
 
-    @FXML
-    void handleResetFilter(ActionEvent event) {
+	 }
 
-    }
+	 @FXML
+	 void handleResetFilter(ActionEvent event) {
+
+	 }
 }
