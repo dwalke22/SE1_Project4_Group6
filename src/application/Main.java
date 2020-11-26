ac681da43b4ce66f478048ec4fac068e64924473
David Walker <thewalker.david@gmail.com>
2020-11-26 12:20:24 -0500

    Specified controller in fxml, gui now shows

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,55 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 
 public class Main extends Application {
+	
+	private static final String WINDOW_TITLE = "Restaurant Picker";
+	private static final String GUI_FXML = "/view/GUI.fxml";
+	
+	/**
+	 * Constructs a new Application object for the Restaurant Picker
+	 * 
+	 * @precondition none
+	 * @postcondition the object is ready to execute
+	 */
+	public Main() {
+		super();
+	}
 	@Override
 	public void start(Stage primaryStage) {
-		//TODO: this doesnt seem to be set up to use the GUI you created.
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Pane pane = this.loadGui();
+			Scene scene = new Scene(pane);
 			primaryStage.setScene(scene);
+			primaryStage.setTitle(WINDOW_TITLE);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+		} catch(IllegalStateException | IOException anException) {
+			anException.printStackTrace();
 		}
 	}
 	
+	private Pane loadGui() throws IOException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource(GUI_FXML));
+		return (Pane) loader.load();
+	}
+	/**
+	 * Launches the application
+	 * 
+	 * @precondition none
+	 * @postcondition none
+	 * 
+	 * @param args
+	 * 			not used
+	 */
 	public static void main(String[] args) {
 		launch(args);
 	}
