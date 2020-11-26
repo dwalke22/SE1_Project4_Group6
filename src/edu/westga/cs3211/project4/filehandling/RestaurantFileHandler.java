344c0e1da1b0375cca0b7aba14b2760e2785c04b
David Walker <thewalker.david@gmail.com>
2020-11-26 12:40:01 -0500

    Fixed checkstyle warnings

--- src/edu/westga/cs3211/project4/filehandling/RestaurantFileHandler.java
+++ src/edu/westga/cs3211/project4/filehandling/RestaurantFileHandler.java
@@ -2,20 +2,9 @@ package edu.westga.cs3211.project4.filehandling;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
 
-import javax.swing.JFileChooser;
-
-import edu.westga.cs3211.project4.formatter.MenuFormatter;
-import edu.westga.cs3211.project4.formatter.RestaurantFormatter;
-import edu.westga.cs3211.project4.model.Menu;
-import edu.westga.cs3211.project4.model.MenuItem;
-import edu.westga.cs3211.project4.model.Restaurant;
 import edu.westga.cs3211.project4.model.RestaurantCollection;
 
 public class RestaurantFileHandler {
